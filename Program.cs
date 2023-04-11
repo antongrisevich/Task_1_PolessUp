7eedf85f3554a8f5ddad21832c20009d770b78e2
antongrisevich <tonuo3553356@gmail.com>
2023-04-11 17:53:33 +0300

    task1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolessUP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 Задание
+
+
+            Console.WriteLine(" +\"\"\"\"\"+ ");
+            Console.WriteLine("(| o o |)");
+            Console.WriteLine(" |  ^  |");
+            Console.WriteLine(" | '-' |");
+            Console.WriteLine(" +-----+");
+        }
+    }
+}
